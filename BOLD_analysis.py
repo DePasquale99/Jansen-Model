01de0e49bd707996d756fd2f1acfbc1b9403f5c1
Daniele De Pasquale <daniele.depasquale2@studio.unibo.it>
2023-05-05 14:38:01 +0200

    Trying the process automatization

--- BOLD_analysis.py
+++ BOLD_analysis.py
@@ -19,7 +19,7 @@ def normalize_data(W):
 
 
 #Loading and preprocessing data: cut the first 20 s due to fucked up initial conditions
-
+'''
 real_data = np.load('Data/real_bold.npy').transpose()
 simulated_data = np.load('Data/desikan_bold.npy')[:,2000:]
 
@@ -54,4 +54,44 @@ print('Correlation coefficient for the connectome and real data:')
 print(pearsonr(W.flatten(), real_R.flatten()))
 
 
+'''
+def data_analysis(path):
+    '''
+    analyzes the data contained in path, using pearson correlation for comparison with the connectome and the real BOLD data
+    acquired experimentally. Returns a tuple containig the correlations of the three pairs of the data
+    '''
 
+    #Loading and preprocessing data: cut the first 20 s due to fucked up initial conditions
+
+    real_data = np.load('Data/real_bold.npy').transpose()
+    simulated_data = np.load(path + '.npy')[:,2000:]
+
+
+    N = len(simulated_data)
+
+    real_R = np.zeros((N,N))
+    simulated_R = np.zeros((N,N))
+    #Calculating pearson correlation coefficient for both data
+    for i in range(N):
+        for j in range(i):
+            R = pearsonr(simulated_data[i], simulated_data[j]).statistic
+            simulated_R[i, j] = R
+            simulated_R[j, i] = R
+            R = pearsonr(real_data[i], real_data[j]).statistic
+            real_R[i, j] = R
+            real_R[j, i] = R
+    #Correlation between the acquired data and the simulated one
+    
+    correlationS =  pearsonr(real_R.flatten(), simulated_R.flatten()).statistic
+    mseS = np.square(np.subtract(real_R.flatten(), simulated_R.flatten())).mean()
+
+    W = np.load('Data/desikan.npy')
+    W = normalize_data(W)
+    #Correlation between the connectome and the simulated data
+    correlationW = pearsonr(W.flatten(), simulated_R.flatten()).statistic
+    mseW = np.square(np.subtract(W.flatten(), simulated_R.flatten())).mean()
+    #Correlation between the acquired data and the connectome
+    correlationC = pearsonr(W.flatten(), real_R.flatten()).statistic
+    mseC = np.square(np.subtract(real_R.flatten(), W.flatten())).mean()
+
+    return (correlationS, correlationW, correlationC, mseS, mseW, mseC)
